253903a1f7f4be2496aa25f28ef7fd7a5cf4601c
thaisfreires <156026093+thaisfreires@users.noreply.github.com>
2024-01-30 18:42:35 +0000

    day_1

--- swap_var.py
+++ swap_var.py
@@ -1,4 +1,4 @@
-#Create a program that swaps the values of two variables.
+#Create a program that swaps the values of two variables
 
 x = input("Enter first value:")
 y = input("Enter second value:")
